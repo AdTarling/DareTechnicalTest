f8b54bc3dc0210790a346a846682d81113d7d44c
Adam Tarling <adamtarling@hotmail.com>
2020-05-21 16:35:25 +0100

    Add jquery

---
+++ Js/HomePageController.js
@@ -0,0 +1,12 @@
+﻿(function (homePageController, $, undefined) {
+    homePageController.initialize = function (options) {
+        this.playlist = options.playlist;
+        console.log(this.playlist);
+    }
+
+    homePageController.someFunctionThatUsesUrl = function () {
+        $.ajax({
+            url: this.someUrl
+        });
+    }
+})(window.homePageController = window.homePageController || {}, jQuery);
